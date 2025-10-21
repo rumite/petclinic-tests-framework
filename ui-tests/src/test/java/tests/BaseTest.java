a174ce2708f92558c798d1833f48bbc6877191e8
rumite <bakhtiyarowtn@gmail.com>
2025-10-22 01:06:11 +0200

    Added headless mode for CI CD

--- ui-tests/src/test/java/tests/BaseTest.java
+++ ui-tests/src/test/java/tests/BaseTest.java
@@ -1,21 +1,32 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUpTest() {
-        setUp();
-        driver.get("http://localhost:8080");
-    }
+    public void setUp() throws IOException {
+        Properties props = new Properties();
+        props.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+        boolean headless = Boolean.parseBoolean(props.getProperty("headless", "true"));
 
-    public void setUp() {
-        driver = new org.openqa.selenium.chrome.ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+
+        if (headless) {
+            options.addArguments("--headless=new");
+        }
+
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:8080");
     }
 
     @AfterMethod
