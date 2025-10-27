5e1ca995484f656caad031c223f53090a5a1fbc1
rumite <bakhtiyarowtn@gmail.com>
2025-10-27 19:53:52 +0100

    Added retrieving 'headless' option for BaseTest.java

--- ui-tests/src/test/java/tests/BaseTest.java
+++ ui-tests/src/test/java/tests/BaseTest.java
@@ -1,27 +1,23 @@
 package tests;
 
+import config.Config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.IOException;
-import java.util.Properties;
-
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() throws IOException {
-        Properties props = new Properties();
-        props.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-        boolean headless = Boolean.parseBoolean(props.getProperty("headless", "true"));
+    public void setUp() {
+        boolean headless = Boolean.parseBoolean(Config.getProperty("headless"));
 
         ChromeOptions options = new ChromeOptions();
 
         if (headless) {
-            options.addArguments("--headless=new");
+            options.addArguments("--headless");
         }
 
         driver = new ChromeDriver(options);
